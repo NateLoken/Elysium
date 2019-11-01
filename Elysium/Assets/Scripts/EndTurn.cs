7ecc59438d8a088ff906697bbea3591139529524
Mason Salcido <mvsvlc@nmsu.edu>
2019-10-31 20:29:20 -0600

    Added End Turn button to rotate game board

---
+++ Elysium/Assets/Scripts/EndTurn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurn : MonoBehaviour
+{
+    public void EndPlayerTurn(float rotationAmount)
+    {
+        transform.Rotate(new Vector3(0, 0, 1), rotationAmount);
+    }
+}
