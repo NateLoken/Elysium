ff55fbfd41f08c0bfda3321fb0ded1fed43e2175
nloken <nfloken@comcast.net>
2019-11-12 17:53:04 -0700

    Added Script to keep track of player turns and number of pieces the player has

---
+++ Elysium/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    private int turn;
+    private int numPieces;
+
+    public Player()
+    {
+        turn = 0;
+        numPieces = 0;
+    }
+
+    public int Turn
+    {
+        get => turn;
+        set => turn = value;
+    }
+
+    public int NumPieces
+    {
+        get => numPieces;
+        set => numPieces = value;
+    }
+}
