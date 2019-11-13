94186637e73d04d402cfbbc66f4063c103f6d0d0
nloken <nfloken@comcast.net>
2019-11-12 18:02:56 -0700

    changed all the getters and setter to public instead of protected

--- Elysium/Assets/Scripts/Pieces/BasePiece.cs
+++ Elysium/Assets/Scripts/Pieces/BasePiece.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasePiece
+public class BasePiece : MonoBehaviour
 {
     private int currentHealth;
     private int maxHealth;
@@ -13,13 +13,13 @@ public class BasePiece
     private int posX;
     private int posY;
 
-    protected int Range { get => range; set => range = value; }
-    protected int Cost { get => cost; set => cost = value; }
-    protected int CurrentHealth { get => currentHealth; set => currentHealth = value; }
-    protected int MaxHealth { get => maxHealth; set => maxHealth = value; }
-    protected int Strength { get => strength; set => strength = value; }
-    protected int Movement { get => movement; set => movement = value; }
-    protected int PosX { get => posX; set => posX = value; }
-    protected int PosY { get => posY; set => posY = value; }
+    public int Range { get => range; set => range = value; }
+    public int Cost { get => cost; set => cost = value; }
+    public int CurrentHealth { get => currentHealth; set => currentHealth = value; }
+    public int MaxHealth { get => maxHealth; set => maxHealth = value; }
+    public int Strength { get => strength; set => strength = value; }
+    public int Movement { get => movement; set => movement = value; }
+    public int PosX { get => posX; set => posX = value; }
+    public int PosY { get => posY; set => posY = value; }
     
 }
