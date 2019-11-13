26a8a03904941ccd2bbee79e373a84cbfa3ea765
nloken <nfloken@comcast.net>
2019-11-12 18:00:35 -0700

    added function to increase player turn count when end turn button is triggered

--- Elysium/Assets/Scripts/EndTurn.cs
+++ Elysium/Assets/Scripts/EndTurn.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 
 public class EndTurn : MonoBehaviour
 {
-    public void EndPlayerTurn(float rotationAmount)
+    public void EndPlayerTurn(float rotationAmount, Player player)
     {
         transform.Rotate(new Vector3(0, 0, 1), rotationAmount);
+        player.Turn += 1;
     }
 }
