59930255f1b20a5363f0728baaf8c9b4157c8d73
nloken <nfloken@comcast.net>
2019-11-15 09:48:24 -0700

    made chess with moving pieces

--- Elysium/Assets/Scripts/Board.cs
+++ Elysium/Assets/Scripts/Board.cs
@@ -1,43 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Copyright (c) 2018 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
-    public GameObject mCellPrefab;
-    [HideInInspector] public Cell[,] mAllCells = new Cell[21,21];
+    public Material defaultMaterial;
+    public Material selectedMaterial;
 
-    public void Create()    
+    public GameObject AddPiece(GameObject piece, int col, int row)
     {
-        for (var y = 0; y < 20; y++)
-        {
-            for (var x = 0; x < 20; x++)
-            {
-                var newCell = Instantiate(mCellPrefab, transform);
-                var rectTransform = newCell.GetComponent<RectTransform>();
-                rectTransform.anchoredPosition = new Vector2((x * 100) + 50, (y * 100) + 50);
-                
-                mAllCells[x,y] = newCell.GetComponent<Cell>();
-                mAllCells[x,y].Setup(new Vector2Int(x, y), this);
-            }
-        }
-
-        for (var x = 0; x < 20; x += 2)
-        {
-            for (var y = 0; y < 20; y++)
-            {
-                var offset = (y % 2 != 0) ? 0 : 1;
-                var finalX = x + offset;
-                
-                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
-            }
-        }
+        Vector2Int gridPoint = Geometry.GridPoint(col, row);
+        GameObject newPiece = Instantiate(piece, Geometry.PointFromGrid(gridPoint),
+            Quaternion.identity, gameObject.transform);
+        return newPiece;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemovePiece(GameObject piece)
     {
-        
+        Destroy(piece);
+    }
+
+    public void MovePiece(GameObject piece, Vector2Int gridPoint)
+    {
+        piece.transform.position = Geometry.PointFromGrid(gridPoint);
+    }
+
+    public void SelectPiece(GameObject piece)
+    {
+        MeshRenderer renderers = piece.GetComponentInChildren<MeshRenderer>();
+        renderers.material = selectedMaterial;
+    }
+
+    public void DeselectPiece(GameObject piece)
+    {
+        MeshRenderer renderers = piece.GetComponentInChildren<MeshRenderer>();
+        renderers.material = defaultMaterial;
     }
 }
